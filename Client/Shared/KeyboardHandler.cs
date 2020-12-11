5015a0399b611410573523c56e4c29cef939bde1
Dmitry Brezhnev <dubtar@gmail.com>
2020-12-11 12:28:41 +0300

    Добавлен обработчик нажатия клавиш на документе

---
+++ Client/Shared/KeyboardHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System;
+
+namespace VeloTiming.Client.Shared
+{
+	public static class KeyboardHandler
+	{
+		public static event EventHandler<KeyboardEvent> OnDocumentKeyDown;
+
+		[JSInvokable]
+		public static void DocumentKeyDown(KeyboardEvent args)
+		{
+			OnDocumentKeyDown?.Invoke(null, args);
+		}
+	}
+
+	public class KeyboardEvent
+	{
+		public string Key { get; set; }
+	}
+}
