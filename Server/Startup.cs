deda27f6bd45bd11292e3c15c1a6fe3de4d1fd83
Dmitry Brezhnev <dubtar@gmail.com>
2021-03-16 14:51:12 +0300

    Старт исправлен

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -45,6 +45,8 @@ namespace VeloTiming.Server
 
 			services.AddSingleton<ITimeService, TimeService>();
 			services.AddSingleton<IRaceLogic, RaceLogic>();
+
+			services.AddScoped<IResultLogic, ResultLogic>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
